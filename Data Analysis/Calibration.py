ceb770da96aa93f39f6263bb7743ee4cbdcf4e2f
uno28 <99043088+uno28@users.noreply.github.com>
2026-06-17 17:16:58 +0900

    fixed Calibration.py

--- Data Analysis/Calibration.py
+++ Data Analysis/Calibration.py
@@ -12,7 +12,7 @@ import ExponentialFit
 class Calibration:
     def __init__(self, V0_tau):
         self.slope = 0.000113781 # THIS ONLY WORKS FOR 1233, 1278, 1288!!!
-        # self.slope = 0.000113781  # THIS ONLY WORKS FOR 1255, 1283, 1293!!!
+        # self.slope = 0.00020634 # THIS ONLY WORKS FOR 1255, 1283, 1293!!!
         self.V0_tau = V0_tau
     def linearFunction(self):
         concentration = self.V0_tau / self.slope
